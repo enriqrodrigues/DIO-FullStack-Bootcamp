17e09562836f15a5d106f7b8432f4510bf538588
enriqRodrigues <mandemsgs@hotmail.com>
2022-01-11 22:15:54 -0300

    "Aula de React"

---
+++ react-components-aula0/src/App.js
@@ -0,0 +1,17 @@
+const App = () => {
+  return(
+    <h1>Hello World</h1>
+  )
+}
+
+export default App;
+
+/*
+export default function App() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+export default App;
+*/
